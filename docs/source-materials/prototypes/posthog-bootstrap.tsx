d46a5f90c2c6be96b555257197970278c84e0a41
Anthony James Padavano <padavano.anthony@gmail.com>
2026-02-12 22:32:27 -0500

    chore(alchemia): ingest posthog-bootstrap.tsx

---
+++ docs/source-materials/prototypes/posthog-bootstrap.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { initPostHog } from "@/lib/analytics";
+
+export function PostHogBootstrap() {
+  useEffect(() => {
+    initPostHog();
+  }, []);
+
+  return null;
+}
